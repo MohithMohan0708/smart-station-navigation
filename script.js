16d63eba8abc8901198fbd81d00c4ae4b20d660f
MohithMohan0708 <mohithmohan0708@gmail.com>
2024-09-04 19:53:06 +0530

    add project files

---
+++ script.js
@@ -0,0 +1,81 @@
+
+let foodcourts = {
+    "latitude" : 18.110826,
+    "longitude" : 83.396129
+};
+let drinkingwater = {
+    "latitude" : 18.111136,
+    "longitude" : 83.396097
+}
+let cloakroom = {
+    "latitude" : 18.110612,
+    "longitude" : 83.396100
+}
+let washroom = {
+    "latitude" : 18.112299,
+    "longitude" : 83.396285
+}
+let waitinghallsNAC = {
+    "latitude" : 18.111751,
+    "longitude" : 83.396084
+}
+let ticketcounter = {
+    "latitude" : 18.111644,
+    "longitude" : 83.396253
+}
+let atms = {
+    "latitude" : 18.111621,
+    "longitude" : 83.396524
+}
+let flyover = {
+    "latitude" : 18.110612,
+    "longitude" : 83.396100
+}
+let autoticketmachine = {
+    "latitude" : 18.111644,
+    "longitude" : 83.396321
+}
+let parcelcounter = {
+    "latitude" : 18.112164,
+    "longitude": 83.396367
+}
+let milkparlour = {
+    "latitude" : 18.111890,
+    "longitude" : 83.396155
+}
+let juiceshop = {
+    "latitude" : 18.111599,
+    "longitude" : 83.396065
+}
+let snackscorner = {
+    "latitude" : 18.111386,
+    "longitude" : 83.396056
+}
+let ticketinspectionoffice = {
+    "latitude" : 18.111599,
+    "longitude" : 83.396106
+}
+let railwaypolicestation = {
+    "latitude" : 18.111052,
+    "longitude" : 83.395811
+}
+let railwaypostoffice = {
+    "latitude" : 18.112615,
+    "longitude" : 83.396034
+}
+let medicalroom = {
+    "latitude" : 18.11263,
+    "longitude" : 83.396102
+}
+let waitinghallAC = {
+    "latitude" : 18.110944,
+    "longitude" : 83.395965
+}
+let govtpolicestation = {
+    "latitude" : 18.112940,
+    "longitude" : 83.396103
+}
+let reservationoffice = {
+    "latitude" : 18.110980,
+    "longitude" : 83.397050
+}
